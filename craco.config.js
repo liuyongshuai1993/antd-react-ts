e9b12b60324e7b6b220d386645138ea2468845f6
ysliu <ysliu@fengyuntec.com>
2020-10-30 14:06:05 +0800

    修改配置

--- craco.config.js
+++ craco.config.js
@@ -4,7 +4,7 @@
  * @Author: 刘永帅
  * @Date: 2020-10-30 09:59:47
  * @LastEditors: 刘永帅
- * @LastEditTime: 2020-10-30 10:50:04
+ * @LastEditTime: 2020-10-30 14:05:22
  */
 const path = require('path')
 const CracoLessPlugin = require('craco-less')
@@ -15,8 +15,36 @@ module.exports = {
 		alias: {
 			'@': path.resolve('src'),
 		},
+		plugins: [],
+	},
+	babel: {
 		plugins: [
+            // 用来支持装饰器
+            ['@babel/plugin-proposal-decorators', { legacy: true }],
+            //按需加载后只需引入组件，无需再额外引入样式文件，
+            //babel会自动按需帮你完成样式的引入。这样打包出来的文件会更小。
+			[
+				'import',
+				{
+                    
+					libraryName: 'antd',
+					libraryDirectory: 'es',
+					style: true, //设置为true即是less
+				},
+			],
 		],
+	},
+	//配置代理解决跨域
+	devServer: {
+		proxy: {
+			'/api': {
+				target: 'http://baidu.com',
+				changeOrigin: true,
+				pathRewrite: {
+					'^/api': '',
+				},
+			},
+		},
 	},
 	plugins: [
 		{
