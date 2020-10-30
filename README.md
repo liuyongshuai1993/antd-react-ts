abe95459465fbcdafa572e7ec55b1fd38c983661
ysliu <ysliu@fengyuntec.com>
2020-10-30 10:53:06 +0800

    删减文件

--- README.md
+++ README.md
@@ -8,4 +8,3 @@
 - 默认支持less  需要安装 craco-less 
 - 使用craco修改create-react-app默认配置 如webpack 自定义主题
 `  modifyVars: { '@primary-color': '#722ed1' }, 定制主题色`
-- 
--- craco.config.js
+++ craco.config.js
@@ -1,24 +1,35 @@
 /*
- * @Descripttion: 
- * @version: 
+ * @Descripttion:
+ * @version:
  * @Author: 刘永帅
  * @Date: 2020-10-30 09:59:47
  * @LastEditors: 刘永帅
- * @LastEditTime: 2020-10-30 10:09:45
+ * @LastEditTime: 2020-10-30 10:50:04
  */
-const CracoLessPlugin = require('craco-less');
+const path = require('path')
+const CracoLessPlugin = require('craco-less')
+const TerserPlugin = require('terser-webpack-plugin')
 module.exports = {
-    plugins: [
-      {
-        plugin: CracoLessPlugin,
-        options: {
-          lessLoaderOptions: {
-            lessOptions: { // 修改主题颜色
-              modifyVars: { '@primary-color': '#722ed1' },
-              javascriptEnabled: true,
-            },
-          },
-        },
-      },
-    ],
-  };
+	webpack: {
+		// 别名
+		alias: {
+			'@': path.resolve('src'),
+		},
+		plugins: [
+		],
+	},
+	plugins: [
+		{
+			plugin: CracoLessPlugin,
+			options: {
+				lessLoaderOptions: {
+					lessOptions: {
+						// 修改主题颜色
+						modifyVars: { '@primary-color': '#722ed1' },
+						javascriptEnabled: true,
+					},
+				},
+			},
+		},
+	],
+}
